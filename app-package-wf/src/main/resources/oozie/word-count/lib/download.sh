ea4954020244fcf4cb80017f71f73f81898872b3
James Clarke <jamclar2@cisco.com>
2016-10-07 17:17:32 +0100

    Download source data files directly from project gutenburg

--- app-package-wf/src/main/resources/oozie/word-count/lib/download.sh
+++ app-package-wf/src/main/resources/oozie/word-count/lib/download.sh
@@ -1,9 +1,9 @@
+set -e
 cd /tmp
 rm -rf lit_tmp_dl
 mkdir lit_tmp_dl
 cd lit_tmp_dl
 wget $1
-tar zxf *.tar.gz
 hadoop fs -rm -r -f -skipTrash $2
 hadoop fs -mkdir -p $2
 hadoop fs -copyFromLocal * $2
