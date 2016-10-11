fe6494b9f6fa5731ad1317299b887c29d38fbb70
James Clarke <jamclar2@cisco.com>
2016-10-11 11:15:11 -0500

    Download source data from s3 again
    because project gutenburg block repeated http requests

--- app-package-wf/src/main/resources/oozie/word-count/lib/download.sh
+++ app-package-wf/src/main/resources/oozie/word-count/lib/download.sh
@@ -4,6 +4,7 @@ rm -rf lit_tmp_dl
 mkdir lit_tmp_dl
 cd lit_tmp_dl
 wget $1
+tar zxf *.tar.gz
 hadoop fs -rm -r -f -skipTrash $2
 hadoop fs -mkdir -p $2
-hadoop fs -copyFromLocal * $2
+hadoop fs -copyFromLocal *.txt $2
